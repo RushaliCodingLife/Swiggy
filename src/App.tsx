dea1c59fd17d51cfdc365babcc1bff134a2ab859
Rushali <rushali.chandora@msbcgroup.com>
2022-12-20 17:23:49 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Routes,
+  Route,Navigate} from "react-router-dom";
+import RestaurantsList from './components/RestaurantsList/RestaurantsList';
+import SignIn from './components/SignIn/SignIn';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+        <Route  path="/" element={<Home />} />
+          <Route  path="/restaurantslist" element={<RestaurantsList />} />
+          <Route  path="/signin" element={<SignIn/>} />
+         <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
